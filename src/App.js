450cd9d450b07baed9ed1aaab635b3b2ee006ac2
Maxime ROBIN <robin.maxime44@gmail.com>
2023-07-06 18:21:58 +0200

    Add presentation to main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./blocks/Home";
+import Presentation from "./blocks/Presentation";
 
 function App() {
   return (
     <div>
       <Home />
+      <Presentation />
     </div>
   );
 }
